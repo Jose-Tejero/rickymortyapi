732a97c2bde5a0a457d4c4289d8a13771d1be024
Jose-Tejero <j_jtejero@hotmail.com>
2022-01-10 04:26:54 -0600

    Recortando solo los elementos que puedan entrar en el vw y anexando la responsividad

--- src/components/ResidentsList.js
+++ src/components/ResidentsList.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ResidentInfo from './ResidentInfo';
 
 const ResidentsList = ({location}) => {
+
+    const [ width, setWidth ] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const changeWidth = () => setWidth(window.innerWidth);
+
+        window.addEventListener( 'resize', changeWidth )
+
+        return () => window.removeEventListener( 'resize', changeWidth );
+    }, []);
+
+    console.log(width);
+
     return (
         <div className='map-info' >
             {
-                location.residents?.map(resident => <ResidentInfo key={resident} resident={resident} locationId={location.id} />)
+                width>480 ? (location.residents?.map(resident => <ResidentInfo key={resident} resident={resident} locationId={location.id} />).slice(0,6)) : (location.residents?.map(resident => <ResidentInfo key={resident} resident={resident} locationId={location.id} />).slice(0,2))
             }
         </div>
     );
